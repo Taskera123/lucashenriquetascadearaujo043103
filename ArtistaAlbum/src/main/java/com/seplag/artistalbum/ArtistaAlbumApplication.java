c6588e372300777ad071dae3fc81fe879bc828dd
Taskera123 <tascalucas6@gmail.com>
2026-01-31 19:37:38 -0400

    feat: implementação do minio

--- ArtistaAlbum/src/main/java/com/seplag/artistalbum/ArtistaAlbumApplication.java
+++ ArtistaAlbum/src/main/java/com/seplag/artistalbum/ArtistaAlbumApplication.java
@@ -3,6 +3,8 @@ package com.seplag.artistalbum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class ArtistaAlbumApplication {
 
     public static void main(String[] args) {
