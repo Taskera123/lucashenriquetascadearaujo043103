d8a316c8bf48f533b49a246001e0c35826422dfc
Taskera123 <tascalucas6@gmail.com>
2026-02-01 09:10:15 -0400

    chore(structure): configuração do facade patern e inicio do projeto frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+// import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
